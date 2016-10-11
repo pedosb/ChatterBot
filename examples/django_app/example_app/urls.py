fb86a75c1b6e92b89cf259a5ad1b8baa5b5d21ca
Pedro Batista <pedosb@gmail.com>
2016-10-11 09:06:38 +0200

    FIES data and initialize URL

--- examples/django_app/example_app/urls.py
+++ examples/django_app/example_app/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
 from example_app.views import ChatterBotAppView
+from example_app.views import ChatterBotInitializeView
 
 
 urlpatterns = [
     url(r'^$', ChatterBotAppView.as_view()),
+    url(r'^initialize$', ChatterBotInitializeView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
 ]
--- examples/django_app/example_app/views.py
+++ examples/django_app/example_app/views.py
@@ -1,6 +1,20 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import View
+from django.http import HttpResponse
 
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot import ChatBot
+
+from chatterbot.ext.django_chatterbot import settings
 
 class ChatterBotAppView(TemplateView):
     template_name = "app.html"
 
+class ChatterBotInitializeView(View):
+    def post(self, request, *args, **kargs):
+        chatterbot = ChatBot(**settings.CHATTERBOT)
+        chatterbot.set_trainer(ChatterBotCorpusTrainer)
+        chatterbot.train("data.fies",
+                         "chatterbot.corpus.portuguese.greetings"
+                        )
+        return HttpResponse("OK", content_type="text/plain")
